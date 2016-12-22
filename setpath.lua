346d8ee6242492a1d0328b875cd1cd819f8499d8
igotyooo <overmars29@gmail.com>
2016-12-22 12:13:06 +0000

    add vgg16 model

--- setpath.lua
+++ setpath.lua
@@ -2,4 +2,6 @@ gpath = { db = {  }, net = {  } }
 gpath.db.ucf101 = '/home/dgyoo/workspace/datain/UCF101FRAME/'
 gpath.net.alex_caffe_model = '/home/dgyoo/workspace/net/caffe/bvlc_alexnet.caffemodel'
 gpath.net.alex_caffe_proto = '/home/dgyoo/workspace/net/caffe/bvlc_alexnet_deploy.prototxt'
+gpath.net.vgg16_caffe_model = '/home/dgyoo/workspace/net/caffe/VGG_ILSVRC_16_layers.caffemodel'
+gpath.net.vgg16_caffe_proto = '/home/dgyoo/workspace/net/caffe/VGG_ILSVRC_16_layers_deploy.prototxt'
 gpath.dataout = '/home/dgyoo/workspace/dataout/large-scale-deep-learning'
